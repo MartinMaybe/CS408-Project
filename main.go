03c87f81e43c1515093c6a4207cf8322f9958d75
MartinMaybe <martingonzalez@u.boisestate.edu>
2026-03-15 17:43:33 -0600

    Landing page layout complete

--- main.go
+++ main.go
@@ -4,34 +4,35 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Page struct {
 	Title string
-	Body  string
+	Time string
 }
 
-var templates = template.Must(template.ParseFiles("hello.html"))
+var templates = template.Must(template.ParseFiles("templates/landing.html"))
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, "hello.html", p)
+func landingHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("landingHandler was called")
+
+	now := time.Now()
+	formatted := now.Format("2006-01-02 15:04")
+	
+	p := &Page{
+		Title: "Public Decision Tree",
+		Time:  formatted,
+	}
+
+	err := templates.ExecuteTemplate(w, "landing.html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("helloHandler was called")
-
-	p := &Page{
-		Title: "Hello",
-		Body:  "World",
-	}
-	renderTemplate(w, "hello.html", p)
-}
-
 func main() {
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/", landingHandler)
 	log.Println("Starting server on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
